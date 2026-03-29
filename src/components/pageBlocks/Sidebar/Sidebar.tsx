04b3489d06a2e935b9d948a75a74746793fd5f9f
partrick <orel.228.337@gmail.com>
2026-03-29 11:06:24 +0300

    Added profile tab

--- src/components/pageBlocks/Sidebar/Sidebar.tsx
+++ src/components/pageBlocks/Sidebar/Sidebar.tsx
@@ -3,23 +3,11 @@ import TabList from "./TabList/TabList";
 
 import s from "./Sidebar.module.css";
 
-// TODO прибрати логаут та рефреш
-import { useLogout } from "../../../hooks/user/useLogout";
-import { refreshToken } from "../../../api/axiosClient";
-
 const Sidebar = () => {
-  const { mutate } = useLogout();
-
-  const onClick = () => {
-    mutate();
-  };
-
   return (
     <aside className={s["sidebar"]}>
       <LogoIcon className={s["logo"]} />
       <TabList />
-      <button onClick={onClick}>LOGOUT</button>
-      <button onClick={refreshToken}>refresh</button>
     </aside>
   );
 };
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,7 +1,50 @@
+import { refreshToken } from "../../api/axiosClient";
+import Button from "../../components/Button/Button";
+import { useLogout } from "../../hooks/user/useLogout";
+import { useUserInfo } from "../../hooks/user/useUserInfo";
+
 import s from "./Profile.module.css";
 
+interface UserInfoAttributeProps {
+  name: string;
+  value: string;
+}
+
+const UserInfoAttribute = ({ name, value }: UserInfoAttributeProps) => {
+  return (
+    <>
+      <p className={s["name"]}>{name}</p>
+      <p className={s["value"]}>{value}</p>
+    </>
+  );
+};
+
 const Profile = () => {
-  return <div>Profile</div>;
+  const { data: userInfo } = useUserInfo();
+
+  const { mutate } = useLogout();
+
+  const onClick = () => {
+    mutate();
+  };
+
+  return (
+    <div className={s["profile-container"]}>
+      <UserInfoAttribute name="Id" value={userInfo.pk} />
+      <UserInfoAttribute name="Email" value={userInfo.email} />
+      <UserInfoAttribute name="Nickname" value={userInfo.nickname} />
+      <UserInfoAttribute
+        name="Joined on"
+        value={new Date(userInfo.created_at).toLocaleString()}
+      />
+      <Button onClick={onClick} className={s["logout-button"]}>
+        Logout
+      </Button>
+      <Button onClick={refreshToken} className={s["refresh-button"]}>
+        Refresh
+      </Button>
+    </div>
+  );
 };
 
 export default Profile;
